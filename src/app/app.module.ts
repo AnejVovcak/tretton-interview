9317d0a7a9ae792f15c62d50fddfd5b5f878a0a6
AnejVovcak <anej.vovcak@gmail.com>
2024-01-25 20:42:24 +0100

    feat(view type): moved view type out of toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {GridOfEmployeesComponent} from "./grid-of-employees/grid-of-employees.component";
 import { ToolBarComponent } from './tool-bar/tool-bar.component';
 import { ListOfEmployeesComponent } from './list-of-employees/list-of-employees.component';
+import { ViewTypeComponent } from './view-type/view-type.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridOfEmployeesComponent,
     ToolBarComponent,
-    ListOfEmployeesComponent
+    ListOfEmployeesComponent,
+    ViewTypeComponent
   ],
   imports: [
     BrowserModule,
