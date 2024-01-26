1f85dbebe76866c87f6b04a3e34b7b452cf8038d
AnejVovcak <anej.vovcak@gmail.com>
2024-01-26 16:40:13 +0100

    feat(UI): some UI improvements

--- src/app/grid-of-employees/grid-of-employees.component.ts
+++ src/app/grid-of-employees/grid-of-employees.component.ts
@@ -6,7 +6,7 @@ import {Employee} from "../../interfaces/employee";
   selector: 'app-grid-of-employees',
   template: `
     <div class="grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
-      <div *ngFor="let employee of employees" class="flex flex-col gap-y-1 border-2 p-2">
+      <div *ngFor="let employee of employees" class="flex flex-col gap-y-1 p-2 border-2 border-air-force-blue">
         <img *ngIf="employee.imagePortraitUrl" [src]="employee.imagePortraitUrl"
              [alt]="'employee'" class="w-full h-2/3 object-cover object-top">
         <img *ngIf="!employee.imagePortraitUrl" src="assets/stock_photo.jpg">
@@ -21,9 +21,6 @@ import {Employee} from "../../interfaces/employee";
 export class GridOfEmployeesComponent implements OnInit {
 
   @Input() employees: Employee[];
-
-  constructor(protected restService: RestService) {
-  }
 
   ngOnInit(): void {
   }
--- src/app/list-of-employees/list-of-employees.component.ts
+++ src/app/list-of-employees/list-of-employees.component.ts
@@ -4,14 +4,14 @@ import {Employee} from "../../interfaces/employee";
 @Component({
   selector: 'app-list-of-employees',
   template: `
-    <div class="grid grid-cols-1 h-28 gap-y-4">
+    <div class="grid grid-cols-1 h-28 gap-y-4 text-light-sky-blue">
       <div *ngFor="let employee of employees" class="flex flex-row items-center gap-x-1 border-2 p-2">
         <div class="w-16 h-16 overflow-hidden">
-<!--          <img *ngIf="employee.imagePortraitUrl"-->
-<!--               [src]="employee.imagePortraitUrl"-->
-<!--               [alt]="'employee'"-->
-<!--               class="rounded-full w-full h-full object-cover">-->
-<!--          <img *ngIf="!employee.imagePortraitUrl" src="assets/stock_photo.jpg" class="rounded-full w-full h-full object-cover">-->
+          <img *ngIf="employee.imagePortraitUrl"
+               [src]="employee.imagePortraitUrl"
+               [alt]="'employee'"
+               class="rounded-full w-full h-full object-cover object-top">
+          <img *ngIf="!employee.imagePortraitUrl" src="assets/stock_photo.jpg" class="rounded-full w-full h-full object-cover">
         </div>
         <div class="flex flex-row gap-x-2">
           <span>{{employee.name}}</span>
